853b03b2225507ac5c1edbe5456904490a0a823e
Panumas20 <panumas20061997@gmail.com>
2022-04-27 23:41:23 +0700

    270422

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const { compile } = require('morgan');
 const debug = require('debug')('server');
 const morgan = require('morgan')
+const path = require('path')
 //const chalk = require('chalk');
 const app  = express();
 const port = 3000;
 
 
 app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname,"/public/")))
+
 app.get("/",(req,res)=>{
     res.send('Hello first')
 })
